614a560e75585ea9bb80bb1b239ce28280a3038d
TonyR <tonyrodriguez26@outlook.com>
2024-06-07 16:40:17 -0400

    finished for each map filter

--- submissionFiles/advancedArrayMethodsPractice/foreach-map-filter.js
+++ submissionFiles/advancedArrayMethodsPractice/foreach-map-filter.js
@@ -192,18 +192,21 @@ Examples:
      // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
 */
 
-function extractFullName(arr)
-{
-    return arr.map(function(element){
+function extractFullName(arr) {
+    return arr.map(function (element) {
         return element.first + " " + element.last;
-    })
+    });
 }
 
-console.log(extractFullName([{first: 'Elie', last:"Schoppik"}, 
-    {first: 'Tim', last:"Garcia"}, 
-    {first: 'Matt', last:"Lane"}, 
-    {first: 'Colt', last:"Steele"}]))
-     // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
+console.log(
+    extractFullName([
+        { first: "Elie", last: "Schoppik" },
+        { first: "Tim", last: "Garcia" },
+        { first: "Matt", last: "Lane" },
+        { first: "Colt", last: "Steele" },
+    ])
+);
+// ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
 
 /*
 Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
@@ -213,8 +216,17 @@ Examples:
 */
 
 function filterByValue(arr, key) {
-
+    return arr.filter(function (element) {
+        return element[key];
+    });
 }
+
+// console.log(filterByValue(
+//     [{first: 'Elie', last:"Schoppik"},
+//         {first: 'Tim', last:"Garcia", isCatOwner: true},
+//         {first: 'Matt', last:"Lane"},
+//         {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner'))
+//         // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]))
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -225,17 +237,45 @@ Examples:
 */
 
 function find(arr, searchValue) {
-    
+    const result = arr.filter(function (element) {
+        return element === searchValue;
+    });
+    return result[0];
 }
+
+console.log(find([1, 2, 3, 4, 5], 10));
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
 
 Examples:
-    findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
+    findInObj([{first: 'Elie', last:"Schoppik"}, 
+    {first: 'Tim', last:"Garcia", isCatOwner: true}, 
+    {first: 'Matt', last:"Lane"}, 
+    {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) 
+
+    // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+    const result = arr.filter(function (element) {
+        return element[key] === searchValue;
+    });
+    return result[0];
+}
+
+console.log(
+    findInObj(
+        [
+            { first: "Elie", last: "Schoppik" },
+            { first: "Tim", last: "Garcia", isCatOwner: true },
+            { first: "Matt", last: "Lane" },
+            { first: "Colt", last: "Steele", isCatOwner: true },
+        ],
+        "isCatOwner",
+        true
+    )
+);
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
@@ -246,7 +286,18 @@ Examples:
     removeVowels('ZZZZZZ') // ('zzzzzz')
 */
 
-function removeVowels(str) {}
+function removeVowels(str) {
+    const strArray = str.toLowerCase().split("");
+    const vowels = "aeiou";
+
+    const result = strArray.filter(function (element) {
+        return !vowels.includes(element);
+    });
+
+    return result.join("");
+}
+
+console.log(removeVowels("Hello World"));
 
 /*
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
@@ -256,4 +307,14 @@ Examples:
     doubleOddNumbers([4,4,4,4,4]) // []
 */
 
-function doubleOddNumbers(arr) {}
+function doubleOddNumbers(arr) {
+    return arr
+        .filter(function (num) {
+            return num % 2 !== 0; // Filter odd numbers
+        })
+        .map(function (oddNum) {
+            return oddNum * 2; // Double the odd numbers
+        });
+}
+
+console.log(doubleOddNumbers([1, 2, 3, 4, 5]));
