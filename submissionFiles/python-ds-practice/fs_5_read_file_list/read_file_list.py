7e573fd544e7377115c12a3107334a53b5773fbb
TonyR <tonyrodriguez26@outlook.com>
2024-08-15 15:23:17 -0400

    finished python exercises

--- submissionFiles/python-ds-practice/fs_5_read_file_list/read_file_list.py
+++ submissionFiles/python-ds-practice/fs_5_read_file_list/read_file_list.py
@@ -16,6 +16,6 @@ def read_file_list(filename):
     It will raise an error if the file cannot be found.
     """
 
-    # hint: when you read lines of files, there will be a "newline"
-    # (end-of-line character) at the end of each line, and you want to
-    # strip that off before you print it. Do some research on that!
+    with open(filename, "r") as file:
+        for line in file:
+            print(f"- {line.strip()}")
