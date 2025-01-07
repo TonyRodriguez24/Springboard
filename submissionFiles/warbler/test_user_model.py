9b69bab3f8beda45b5c19c575eaf7758a81fe03e
TonyRodriguez26 <tonyrodriguez26@outlook.com>
2025-01-06 19:21:39 -0500

    test user model works

--- submissionFiles/warbler/test_user_model.py
+++ submissionFiles/warbler/test_user_model.py
@@ -15,7 +15,7 @@ from models import db, User, Message, Follows
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
@@ -26,8 +26,6 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-db.create_all()
-
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
@@ -35,11 +33,11 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
 
-        self.client = app.test_client()
+            self.client = app.test_client()
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -50,9 +48,11 @@ class UserModelTestCase(TestCase):
             password="HASHED_PASSWORD"
         )
 
-        db.session.add(u)
-        db.session.commit()
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        with app.app_context():
+            db.session.add(u)
+            db.session.commit()
+
+            # User should have no messages & no followers
+            self.assertEqual(len(u.messages), 0)
+            self.assertEqual(len(u.followers), 0)
