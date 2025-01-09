a523d6087e1357632d5a42923976aa9d10dcb6a0
TonyRodriguez26 <tonyrodriguez26@outlook.com>
2025-01-08 21:44:53 -0500

    refactored with ternary operator

--- submissionFiles/react-jsx-intro/part-3/Person.js
+++ submissionFiles/react-jsx-intro/part-3/Person.js
@@ -1,16 +1,12 @@
 const Person = (props) => {
-    let str = '';
-    let reply;
+    const reply = props.age > 18
+        ? <h3>please go vote</h3>
+        : <h3>you must be 18</h3>;
+    
 
-    if (props.age > 18) {
-        reply = <h3>please go vote</h3>
-    } else {
-        reply = <h3>you must be 18</h3>
-    }
-
-    if (props.name.length > 8) {
-        str = props.name.substring(0, 6)
-    }
+    const str = props.name.length < 8
+        ? props.name
+        : props.name.substring(0, 6);
 
     return (
         <div>
