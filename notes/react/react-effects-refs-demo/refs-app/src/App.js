9eccca8bf4240666452e7371e456eb4a5754a2dc
TonyRodriguez26 <tonyrodriguez26@outlook.com>
2025-02-06 15:36:12 -0500

    use effect intro

---
+++ notes/react/react-effects-refs-demo/refs-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Timer from "./Timer";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Video /> */}
+      {/* <TimerWrapper /> */}
+      <Timer />
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
