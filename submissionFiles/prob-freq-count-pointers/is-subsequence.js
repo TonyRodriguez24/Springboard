75a3ae1d246bfc168bec9b1313bbb97b3baff2e1
Tony Rodriguez <tonyrodriguez26@outlook.com>
2025-02-26 18:00:47 -0500

    added ex to problems to finish later

--- submissionFiles/prob-freq-count-pointers/is-subsequence.js
+++ submissionFiles/prob-freq-count-pointers/is-subsequence.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+// Takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. 
+// Function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+function isSubsequence(str1, str2) {
+    
 }
+
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
