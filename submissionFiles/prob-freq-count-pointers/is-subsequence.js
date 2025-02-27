67e726822da85030a41c6a8f4a91d43086ac5304
Tony <tonyrodriguez26@outlook.com>
2025-02-26 21:33:02 -0500

    finished is subsequence and separate positive

--- submissionFiles/prob-freq-count-pointers/is-subsequence.js
+++ submissionFiles/prob-freq-count-pointers/is-subsequence.js
@@ -1,12 +1,32 @@
 // add whatever parameters you deem necessary
-// Takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. 
+// Takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string.
 // Function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+let str1 = 'fasdfasd'
+
+
 function isSubsequence(str1, str2) {
-    
+    if (str2.includes(str1)) return true;
+
+    let arr = str1.split('');
+    let arr2 = str2.split('');
+
+    let left = 0;
+    let right = arr.length - 1;
+
+
+    while (left < right) {
+        
+    }
+
+
+    return false;
 }
 
 
-isSubsequence('hello', 'hello world'); // true
-isSubsequence('sing', 'sting'); // true
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting') );
+
+
 isSubsequence('abc', 'abracadabra'); // true
 isSubsequence('abc', 'acb'); // false (order matters)
--- submissionFiles/prob-freq-count-pointers/separate-positive.js
+++ submissionFiles/prob-freq-count-pointers/separate-positive.js
@@ -1,15 +1,64 @@
-// Write a function called separatePositive which accepts an array of non-zero integers. 
-// Separate the positive integers to the left and the negative integers to the right. 
-// The positive numbers and negative numbers need not be in sorted order. 
+// Write a function called separatePositive which accepts an array of non-zero integers.
+// Separate the positive integers to the left and the negative integers to the right.
+// The positive numbers and negative numbers need not be in sorted order.
 // The problem should be done in place (in other words, do not build a copy of the input array).
 
 
+let num = [3, 4, 5, 10, 32, 32]
+let removed = num.splice(2, 1)
+let test = num.push(removed[0])
+console.log(num)
+console.log(test)
+
+
+
+
+
+// function separatePositive(arr) {
+//     let left = 0
+//     let right = arr.length - 1;
+
+//     while (left < right) {
+//         if (arr[left] < 0) {
+//             let removed = arr.splice(left, 1);
+//             arr.push(removed[0]);
+//             right--; //have to reduce the size of the array
+//         } else{
+//             left++;
+
+//         }
+//     }
+//     return arr;
+// }
+
+
+//swap them in place instead
 
 function separatePositive(arr) {
-    
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        if (arr[left] < 0 && arr[right] > 0)
+        //if value at start is negative and value at right is positve
+        {
+            //immediately swap them in place, this is array destructuring
+            [arr[left], arr[right]] = [arr[right], arr[left]]
+            left++; //move left to the right and right to the left, check the next two values
+            right--;
+        } else {
+            if (arr[left] > 0) left++;
+            if (arr[right] < 0) right--;
+        }
+    }
+    return arr;
+
 }
 
-separatePositive([2, -1, -3, 6, -8, 10]) // [2, 10, 6, -3, -1, -8]
+
+
+
+console.log(separatePositive([2, -1, -3, 6, -8, 10])) // [2, 10, 6, -3, -1, -8]
 separatePositive([5, 10, -15, 20, 25]) // [5, 10, 25, 20, -15]
 separatePositive([-5, 5]) // [5, -5]
 separatePositive([1, 2, 3]) // [1, 2, 3]
