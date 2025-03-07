da7908716bbed95d18f7176b2424f194c02bdc25
Tony <tonyrodriguez26@outlook.com>
2025-03-07 15:24:59 -0500

    outcomes js tricky parts

--- submissionFiles/outcomes-js-tricky-parts/create-account.js
+++ submissionFiles/outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,59 @@
 function createAccount(pin, amount) {
+    let registeredPin = pin;
+    let balance = amount;
+
+    function checkBalance(pin) {
+        if (pin === registeredPin) {
+            return balance
+        }
+        else {
+            return "Access denied"
+        }
+    }
+
+    function deposit(pin, amount) {
+        if (pin === registeredPin) {
+            balance += amount;
+            return `Successfully deposited ${amount}, your new balance is ${balance}`
+        }
+        else {
+            return 'Access denied'
+        }
+    }
+
+    function withdraw(pin, amount) {
+        if (pin === registeredPin) {
+            balance -= amount;
+            return `Successfully withdrew ${amount}, your new balance is ${balance}`
+        }
+        else {
+            return 'Access denied'
+        }
+    }
+
+    function changePin(pin, newPin) {
+            if (pin === registeredPin) {
+                registeredPin = newPin;
+                return `Your new PIN is ${registeredPin}`
+            }
+            else {
+                return 'Access denied'
+            }
+        
+    }
+
+
+    return {checkBalance, withdraw, deposit, changePin}
+
 
 }
 
 module.exports = { createAccount };
+
+let account = createAccount('1234', 100);
+console.log(account)
+
+console.log(account.checkBalance('1234'))
+console.log(account.deposit('1234', 300))
+console.log(account.withdraw('1234', 100))
+console.log(account.changePin('1234' , '5678'))
--- submissionFiles/outcomes-js-tricky-parts/guessing-game.js
+++ submissionFiles/outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,37 @@
 function guessingGame() {
+    let guesses = 1;
 
+    function game(num){
+        const randomNum = Math.floor(Math.random() * 100)
+
+        if (num === randomNum) {
+            return `You win, you found ${randomNum} in ${guesses} guesses`
+        }
+
+        if (num < randomNum) {
+            guesses += 1;
+            return `${num} is too low! Guess higher`
+        }
+
+        if (num > randomNum) {
+            guesses += 1;
+            return `${num} is too high! Guess lower`
+        }
+        
+    }
+
+    return game;
 }
+
+
+let guess = guessingGame();
+console.log(guess(40))
+console.log(guess(50))
+console.log(guess(60))
+console.log(guess(70))
+console.log(guess(80))
+console.log(guess(55))
+console.log(guess(90))
+
 
 module.exports = { guessingGame };
