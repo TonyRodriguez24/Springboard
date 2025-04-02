974761c65395a5d393027bca9b3c7b5c408108f5
Tony <tonyrodriguez26@outlook.com>
2025-04-02 13:21:48 -0400

    finished express companies invoices practice

--- submissionFiles/express-biztime/routes/invoices.js
+++ submissionFiles/express-biztime/routes/invoices.js
@@ -40,7 +40,10 @@ router.post('/', async (req, res, next) => {
 //updates an invoice
 router.put('/:id', async (req, res, next) => {
     try {
-        const result = await db.query(`UPDATE invoices SET `)
+        const { id } = req.params;
+        const { amt, paid, add_date, paid_date } = req.body;
+        const result = await db.query(`UPDATE invoices SET amt=$1, paid=$2, add_date=$3, paid_date=$4 WHERE id=$5 RETURNING * `, [amt, paid, add_date, paid_date, id])
+        return res.json(result.rows[0])
     } catch (error) {
         return next(error)
     }
@@ -49,7 +52,9 @@ router.put('/:id', async (req, res, next) => {
 //deletes an invoice
 router.delete('/:id', async (req, res, next) => {
     try {
-        
+        const { id } = req.params;
+        const result = await db.query('DELETE FROM invoices WHERE id=$1', [id])
+        return res.json({message: 'Invoice successfully deleted'})
     } catch (error) {
         return next(error)
     }
