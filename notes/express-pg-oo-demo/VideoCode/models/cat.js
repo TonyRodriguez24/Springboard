667611c1e2d28aa0e8458bc4c95d7a1444182ee9
Tony <tonyrodriguez26@outlook.com>
2025-04-07 12:21:25 -0400

    finished cats simple oo approach, starting dog one closer to SQLAlchemy

--- notes/express-pg-oo-demo/VideoCode/models/cat.js
+++ notes/express-pg-oo-demo/VideoCode/models/cat.js
@@ -1,63 +1,42 @@
 const db = require("../db");
 const ExpressError = require("../expressError")
 
+
+//for models make all static methods
 class Cat {
   static async getAll() {
-    let result = await db.query("SELECT id, name, age FROM cats");
-    return result.rows;
+    const result = await db.query('SELECT id, name, age FROM cats;')
+    return result.rows
   }
-  static async getById(id) {
-    const result = await db.query(`SELECT id, name, age FROM cats WHERE  id = $1`, [id]);
-    if (result.rows.length === 0) {
-      throw new ExpressError("Cat not found", 404)
-    }
+
+  static async getCatById(id) {
+    const result = await db.query(`SELECT id, name, age FROM cat WHERE id =$1`, [id])
+    if (result.rows.length === 0) throw new ExpressError('Cat not found', 404);
     return result.rows[0];
   }
+
   static async create(name, age) {
-    if (!name || !age) {
-      throw new ExpressError("Missing required data", 400);
-    }
-    const result = await db.query(
-      `INSERT INTO cats (name, age) 
-      VALUES ($1, $2)
-      RETURNING id, name, age`,
-      [name, age]);
-    return result.rows[0];
+    if (!name || !age) throw new ExpressError('Missing required data', 400); //400 is bad request
+    const result = await db.query(`INSERT INTO cats (name, age) VALUES ($1, $2) RETURNING id, name, age`, [name, age])
+    return result.rows[0]
   }
+
   static async delete(id) {
-    const result = await db.query(`
-      DELETE FROM cats
-      WHERE id = $1
-      RETURNING id`,
-      [id])
-    if (result.rows.length === 0) {
-      throw new ExpressError("Cat not found", 404)
-    }
+    const result = await db.query(`DELETE from cats WHERE id = $1 RETURNING id`, [id]) // we only return the id here just to check if its empty or not in the following line
+    if (result.rows.length === 0) throw new ExpressError('Cat not found', 404)
   }
+
   static async update(id, newName, newAge) {
-    const result = await db.query(`
-      UPDATE cats SET name = $1, age  = $2 
-      WHERE id = $3
-      RETURNING id, name, age
-    `, [newName, newAge, id])
-    if (result.rows.length === 0) {
-      throw new ExpressError("Cat not found", 404)
-    }
+    const result = await db.query(`UPDATE cats SET name = $1, age =$2 WHERE id = $3 RETURNING id, name, age`, [newName, newAge, id])
+    if (result.rows.length === 0) throw new ExpressError('Cat not found', 404)
     return result.rows[0]
   }
 
   static async makeOlder(id) {
-    const result = await db.query(`
-      UPDATE cats SET age  = age + 1
-      WHERE id = $1
-      RETURNING id, name, age
-    `, [id])
-    if (result.rows.length === 0) {
-      throw new ExpressError("Cat not found", 404)
-    }
+    const result = await db.query(`UPDATE cats SET age = age + 1 WHERE id = $1 RETURNING id, name, age`, [id])
+    if (result.rows.length === 0) throw new ExpressError('Cat not found', 404)
     return result.rows[0]
   }
-
 
 }
 module.exports = Cat;
--- notes/express-pg-oo-demo/VideoCode/routes/cats.js
+++ notes/express-pg-oo-demo/VideoCode/routes/cats.js
@@ -5,61 +5,67 @@ const router = new express.Router();
 
 
 /** get all cats: [{id, name, age}, ...] */
-router.get("/", async function (req, res, next) {
+router.get('/', async (req, res, next) => {
   try {
-    const cats = await Cat.getAll()
+    const cats = await Cat.getAll();
     return res.json(cats)
-  } catch (e) {
-    return next(e)
+  } catch (error) {
+    return next(error)
   }
-});
 
+})
+
+//have to await it because it returns a promise
 router.get('/:id', async (req, res, next) => {
   try {
-    const cat = await Cat.getById(req.params.id);
-    return res.json(cat);
-  } catch (e) {
-    return next(e)
+    const cat = await Cat.getCatById(req.params.id);
+    return res.json(cat)
+  } catch (error) {
+    return next(error)
   }
+
 })
+
+
 
 router.post('/', async (req, res, next) => {
   try {
-    const { name, age } = req.body;
-    const cat = await Cat.create(name, age)
-    return res.json(cat);
-  } catch (e) {
-    return next(e)
+    const { name, age } = req.body
+    const newCat = await Cat.create(name, age)
+    return res.json(newCat)
+  } catch (error) {
+    return next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const {name, age} = req.body
+    const cat = await Cat.update(req.params.id, name, age)
+    return res.json(cat)
+  } catch (error) {
+    return next(error)
   }
 })
 
 router.delete('/:id', async (req, res, next) => {
   try {
     await Cat.delete(req.params.id)
-    return res.json({ msg: "Deleted" })
-  } catch (e) {
-    return next(e)
-  }
-})
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { name, age } = req.body;
-    const cat = await Cat.update(req.params.id, name, age)
-    return res.json(cat)
-  } catch (e) {
-    return next(e)
+    return res.json({ message: 'Deleted'})
+  } catch (error) {
+    return next(error)
   }
 })
 
 router.patch('/:id', async (req, res, next) => {
   try {
-    const cat = await Cat.makeOlder(req.params.id);
+    const cat = await Cat.makeOlder(req.params.id)
     return res.json(cat)
-  } catch (e) {
-    return next(e)
+  } catch (error) {
+    return next(error)
   }
 })
+
 
 module.exports = router;
 
