d38e2f0ca2750062f1e996713641a534e953288d
Tony <tonyrodriguez26@outlook.com>
2025-04-07 13:20:22 -0400

    finished express mini orm practice

--- notes/express-pg-oo-demo/VideoCode/models/dog.js
+++ notes/express-pg-oo-demo/VideoCode/models/dog.js
@@ -2,6 +2,45 @@ const db = require("../db");
 const ExpressError = require("../expressError");
 
 
-class Dog {}
+class Dog {
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age
+    }
+
+    static async getAll() {
+        const results = await db.query(`SELECT id, name, age FROM dogs`)
+        console.log(results.rows)
+        const dogs = results.rows.map(row => new Dog(row.id, row.name, row.age))
+        console.log(dogs)
+        return dogs;
+    }
+
+    static async getById(id) {
+        const results = await db.query(`SELECT id, name, age FROM dogs WHERE id =$1`, [id])
+        const d = results.rows[0]
+        if(!d) throw new ExpressError('Dog not found', 404)
+        return new Dog(d.id, d.name, d.age)
+    }
+
+    static async create(newName, newAge) {
+        const results = await db.query(`INSERT INTO dogs (name, age) VALUES ($1, $2) RETURNING id, name, age`, [newName, newAge])
+        const { id, name, age } = results.rows[0]
+        return new Dog(id, name, age);
+    }
+
+    async remove() {
+        await db.query(`DELETE FROM dogs WHERE id=$1`, [this.id])
+    }
+
+    async save() {
+        await db.query(`UPDATE dogs SET name =$1, age = $2 where id = $3 RETURNING id, name, age`, [this.name, this.age, this.id])
+    }
+
+    speak() {
+        console.log(`${this.name} says woof`)
+    }
+}
 
 module.exports = Dog;
--- notes/express-pg-oo-demo/VideoCode/routes/dogs.js
+++ notes/express-pg-oo-demo/VideoCode/routes/dogs.js
@@ -3,6 +3,76 @@ const router = new express.Router();
 const Dog = require("../models/dog");
 
 
+router.get('/', async (req, res, next) => {
+  try {
+    const dogs = await Dog.getAll();
+    dogs.forEach(d => d.speak())
+    return res.json(dogs)
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const dog = await Dog.getById(req.params.id);
+    return res.json(dog)
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, age } = req.body
+    const dog = await Dog.create(name, age)
+    return res.json(dog)
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const dog = await Dog.getById(req.params.id)
+    await dog.remove()
+    return res.json({
+      message: 'Dog deleted'
+    })
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
+router.patch('/:id/age', async (req, res, next) => {
+  try {
+    const dog = await Dog.getById(req.params.id)
+    dog.age += 1;
+    await dog.save()
+    return res.json(dog)
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
+router.patch('/:id/rename', async (req, res, next) => {
+  try {
+    const dog = await Dog.getById(req.params.id)
+    dog.name = req.body.name;
+    await dog.save()
+    return res.json(dog)
+  } catch (error) {
+    return next(error)
+  }
+
+})
+
 
 
 
