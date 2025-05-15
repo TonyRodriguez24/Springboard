0f9721a81e3312a0b700c25d6c512facc7a44160
Tony <tonyrodriguez26@outlook.com>
2025-05-15 17:31:22 -0400

    dsa trees

--- submissionFiles/dsa-trees/tree.js
+++ submissionFiles/dsa-trees/tree.js
@@ -15,27 +15,59 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    if (!this.root) return 0; // if it is null then there is no root node
+    if (!this.root) return 0;
+
     let total = 0;
-    total += this.root.val;
-    for (let child of this.root.children) {
-      sumValues();
+
+    function dfs(node) {
+      total += node.val;
+      for (let child of node.children) {
+        dfs(child);
+      }
     }
+
+    dfs(this.root);
     return total;
   }
-
+  
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) return 0;
 
+    let count = 0;
+
+    function dfs(node) {
+      if (node.val % 2 === 0) count++;
+      for (let child of node.children) {
+        dfs(child);
+      }
+    }
+
+    dfs(this.root);
+    return count;
   }
+  
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) return 0;
 
+    let count = 0;
+
+    function dfs(node) {
+      if (node.val > lowerBound) count++;
+      for (let child of node.children) {
+        dfs(child);
+      }
+    }
+
+    dfs(this.root);
+    return count;
   }
+  
 }
 
 
